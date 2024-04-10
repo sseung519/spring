51f2df366e09df4d9c415e828818fcafecabc422
sseung <htc_desirhed@naver.com>
2024-04-10 15:35:41 +0900

    .

--- game/src/main/java/com/example/game/controller/MemberController.java
+++ game/src/main/java/com/example/game/controller/MemberController.java
@@ -14,23 +14,23 @@ public class MemberController {
     @Autowired
     MemberService memberService;
 
-    @GetMapping(value= "/login") // localhost/login
+    @GetMapping(value = "/login") // localhost/login
     public String login() {
         return "member/login";
     }
 
-    @PostMapping(value="/login")
-    public String loginMember(Member member, HttpSession session) { // 로그인 처리
+    @PostMapping(value = "/login")
+    public String loginMember(Member member, HttpSession session) {
         //1. 사용자가 입력한 로그인 데이터와 DB에 저장된 데이터가 맞는지 비교
         try {
             Member loginMember = memberService.loginMember(member);
             //2. 데이터가 일치하면(로그인 성공시) index페이지로 이동
 
-            if(loginMember != null) {
+            if (loginMember != null) {
                 //로그인 성공시 로그인한 사람의 name과 member_id를 세션에 저장
                 // .setAttribute(키, 값) -> 세션에 값을 저장
                 session.setAttribute("name", loginMember.getName());
-                session.setAttribute("member_id",loginMember.getMemberId());
+                session.setAttribute("member_id", loginMember.getMemberId());
                 // balance: 잔고 값 표시를 위해 값 저장
                 session.setAttribute("balance", loginMember.getBalance());
                 return "redirect:/";
@@ -38,11 +38,11 @@ public class MemberController {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        
+
         return "member/login";
     }
 
-    @GetMapping(value="/logout") // localhost/logout
+    @GetMapping(value = "/logout") // localhost/logout
     public String logoutMember(HttpSession session) {
         //세션에 저장된 name과 memer_id 삭제
         session.removeAttribute("name");
@@ -52,17 +52,24 @@ public class MemberController {
         return "redirect:/"; // 로그아웃 성공
     }
 
-    @PostMapping(value="/charge")
+
+    @GetMapping(value = "/charge")
+    public String ChargeBalance() {
+        return "member/charge";
+    }
+
+    @PostMapping(value = "/charge")
     public String chargeBalance(@RequestParam("charge") int charge, HttpSession session) {
         try {
             int memberId = (int) session.getAttribute("member_id");
             Member member = new Member();
             member.setMemberId(memberId);
-            member.setBalance(charge);
             memberService.chargeBalance(member, charge);
+            int updatedBalance = ((int) session.getAttribute("balance")) + charge;
+            session.setAttribute("balance", updatedBalance);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return "member/charge";
+        return "redirect:/member/charge";
     }
 }
